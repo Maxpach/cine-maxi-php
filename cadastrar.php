e7317590ff6248775ca0df57aff3eddccda0984a
Maxpach <32625973+Maxpach@users.noreply.github.com>
2022-02-25 18:34:38 -0300

    update

--- cadastrar.php
+++ cadastrar.php
@@ -11,60 +11,64 @@
                 <li><a href="cadastrar.php">Cadastrar</a></li>
             </ul>
         </div>
-        <div class="nav-header center " >
+        <div class="nav-header center ">
             <h1 class="cine">CINEMAX</h1>
         </div>
     </nav>
 
     <div class="row">
-        <div class="col s6 offset-s3">
-            <div class="card blue-grey darken-1">
-                <div class="card-content white-text">
-                    <span class="card-title">Cadastrar Filme</span>
-
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="titulo" type="text" class="validate">
-                            <label for="titulo">Titulo do Filme</label>
+        <form action="inserirFilme.php">
+            <div class="col s6 offset-s3">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">Cadastrar Filme</span>
+                        
+                        <!--Input titulo -->
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="titulo" type="text" class="validate" name="titulo">
+                                <label for="titulo">Titulo do Filme</label>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="row">
-                        <form class="col s12">
+                        <!--Input sinopse -->
+                        <div class="row">
                             <div class="row">
                                 <div class="input-field col s12">
                                     <textarea id="sinopse" class="materialize-textarea"></textarea>
                                     <label for="sinopse">Sinopse</label>
                                 </div>
-                        </form>
+                            </div>
+                        </div>
+
+                        <!--Input nota -->
+                        <div class="row">
+                            <div class="input-field col s4">
+                                <input id="nota" type="number" step=",1" min=0 max=10 class="validate">
+                                <label for="nota">Nota</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--Input capa -->
+                    <div class="file-field input-field ">
+                        <div class="btn amber lighten-1 black-text">
+                            <span>Capa</span>
+                            <input type="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text">
+                        </div>
+                    </div>
+
+                    <!--button voltar -->
+                    <div class="card-action">
+                        <a class="btn waves-effect waves-light btn grey" href="galeria.php">Cancelar</a>
+                        <button type="submit" href="#" class="waves-effect waves-light btn amber darken">Confirmar</button>
                     </div>
                 </div>
-
-                <div class="row">
-                    <div class="input-field col s4">
-                        <input id="nota" type="number" step=",1" min=0 max=10 class="validate">
-                        <label for="nota">Nota</label>
-                    </div>
-                </div>
             </div>
-
-            <div class="file-field input-field ">
-                <div class="btn amber lighten-1 black-text" >
-                    <span>Capa</span>
-                    <input type="file">
-                </div>
-                <div class="file-path-wrapper">
-                    <input class="file-path validate" type="text">
-                </div>
-            </div>
-
-
-            <div class="card-action">
-                <a class="btn waves-effect waves-light btn grey" href="galeria.php">Cancelar</a>
-                <a href="#" class="waves-effect waves-light btn amber darken">Confirmar</a>
-            </div>
-        </div>
-    </div>
+        </form>
     </div>
 
 </body>
--- galeria.php
+++ galeria.php
@@ -80,32 +80,32 @@ $filme6= [  "titulo"=>"Batman",
             </ul>
         </div>
     </nav>
-
-     <!--CARD-->
-    <div class="row">
-    <!--outomatizado /*($i=0; $i<count($filmes); $i++) { 
-                $filme = $filmes[$i];*/
-        ?php foreach($filmes as $filme)--> 
-        
-        <?php while ($filme = $filmes->fetchArray()): ?>        
-            <div class="col s3">
-            <div class="card hoverable">
-                <div class="card-image">
-                    <img src="<?= $filme["poster"]?>">
-                    <a class="btn-floating halfway-fab waves-effect waves-light red">
-                        <i class="material-icons">favorite_border</i></a>
-                </div>
-                <div class="card-content">
-                    <p class="valign-wrapper">                 
-                        <i class="material-icons amber-text">star</i><?= $filme["nota"]?>
-                    </p>
-                    <span class="card-title"> <?= $filme["titulo"]?> </span>
-                    <p><?= $filme["sinopse"]?></p>
+    <div class="container">
+        <!--CARD-->
+        <div class="row">
+        <!--outomatizado /*($i=0; $i<count($filmes); $i++) { 
+                    $filme = $filmes[$i];*/
+            ?php foreach($filmes as $filme)--> 
+            
+            <?php while ($filme = $filmes->fetchArray()) : ?>        
+                <div class="col s12 m6 l3">
+                <div class="card hoverable">
+                    <div class="card-image">
+                        <img src="<?= $filme["poster"]?>">
+                        <a class="btn-floating halfway-fab waves-effect waves-light red">
+                            <i class="material-icons">favorite_border</i></a>
+                    </div>
+                    <div class="card-content">
+                        <p class="valign-wrapper">                 
+                            <i class="material-icons amber-text">star</i><?= $filme["nota"]?>
+                        </p>
+                        <span class="card-title"> <?= $filme["titulo"]?> </span>
+                        <p><?= $filme["sinopse"]?></p>
+                    </div>
                 </div>
             </div>
+            <?php endwhile ?>
         </div>
-        <?php endwhile ?>
-
     </div>
     <!--Botão
     <a href="#" class="waves-effect waves-light btn">Button</a>-->
