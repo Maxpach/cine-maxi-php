273d175875f9961167864875063c28da6ffff0a3
Maxpach <32625973+Maxpach@users.noreply.github.com>
2022-02-24 22:52:10 -0300

    add cards

--- galeria.php
+++ galeria.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,9 +10,73 @@
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         
-    <title>Cine_Maxi</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link href="./syle.css" rel="stylesheet">
+
+    <title>CineMaxi</title>
+
 </head>
 <body>
-    
+
+    <!--HEAD-->
+    <nav class="nav-extended blue-grey lighten-2">
+        <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.html">Galeria</a></li>
+                <li><a href="cadastrar.html">Cadastrar</a></li>
+            </ul>
+        </div>
+        <div class="nav-header center">
+            <h1>CINEMAX</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent blue-grey darken-4">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                <li class="tab"><a href="#test2">Assistidos</a></li>
+                <li class="tab"><a href="#test3">Favoritos</a></li>
+            </ul>
+        </div>
+    </nav>
+
+     <!--CARD-->
+    <div class="row">
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/qJQknP1F9R4pS5qiOuvpIUuWam4.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i></a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">                 
+                        <i class="material-icons amber-text">star</i>9,7
+                    </p>
+                    <span class="card-title">Vingadores</span>
+                    <p>Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.</p>
+                </div>
+            </div>
+
+        <div class="col-s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://www.themoviedb.org/t/p/w300/doiUtOHzcxXFl0GVQ2n8Ay6Pirx.jpg">                   
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i></a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i>9,9
+                    </p>
+                    <span class="card-title">Batman vs Superman</span>
+                    <p>Temendo que as ações de um super-herói divino não tenham sido controladas, o formidável e vigoroso vigilante de Gotham City enfrenta o salvador mais reverenciado e moderno de Metrópolis, enquanto o mundo luta com o tipo de herói que realmente precisa.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Botão
+    <a href="#" class="waves-effect waves-light btn">Button</a>-->
+
 </body>
 </html>
