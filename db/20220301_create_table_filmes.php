e939e879dac83cc00d9494bc5978af446d9a58d9
unknown <leandromsca@hotmail.com>
2022-04-05 18:47:57 -0300

    teste config

--- db/20220301_create_table_filmes.php
+++ db/20220301_create_table_filmes.php
@@ -10,11 +10,11 @@ if ($bd->exec($sql))
 */
 
 $sql = "CREATE TABLE filmes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
         titulo VARCHAR(200) NOT NULL,
         poster VARCHAR(200),
         sinopse TEXT,
-        nota DECIMAL(2,1) 
+        nota DECIMAL(3,1) 
     )
 ";
 /*
