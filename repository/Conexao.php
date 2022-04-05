0b31bff80c4363a5980501582179f0cf0b37ac57
unknown <leandromsca@hotmail.com>
2022-04-05 18:32:13 -0300

    configurando bdo

--- repository/Conexao.php
+++ repository/Conexao.php
@@ -1,7 +1,18 @@
 <?php
 class Conexao{
     public static function criar():PDO{
-        return new PDO("sqlite:db/filmes.db");
+        $env = parse_ini_file('.env');
+        $databaseType = $env["databasetype"];
+        $database = $env["database"];
+        $server = $env["server"];
+        $pass = $env["pass"];
+        $user = $env["user"];
+
+        if($databaseType === "mysql"){
+            $database = "host=$server;dbname=$database";
+        }
+
+        return new PDO("$databaseType:$database", $user, $pass);
     }
 }
 
